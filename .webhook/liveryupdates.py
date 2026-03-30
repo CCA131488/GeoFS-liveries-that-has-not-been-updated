5df551d552ec0c8fb66d0554ad8c4afcdd7e53db
CP8888 <2756483177@qq.com>
2026-03-30 20:53:54 +0800

    Update liveryupdates.py

--- .webhook/liveryupdates.py
+++ .webhook/liveryupdates.py
@@ -3,33 +3,18 @@ import json
 import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-WEBHOOK = os.environ["LIVERYUPDATES"]
+LIVERY_UPDATE_WEBHOOK = os.environ["LIVERYUPDATES"]
 
-commit_file = ".webhook/commit.txt"
+with open(".webhook/commit.txt", "r") as file:
+    commit_id = file.read()
+    print(commit_id)
 
-# 读取 commit.txt 获取上次的 commit id
-if os.path.exists(commit_file):
-    with open(commit_file, "r") as f:
-        commit_id = f.read().strip()
-else:
-    commit_id = None
+new_json =  json.loads(requests.get("https://raw.githubusercontent.com/CCA131488/
+GeoFS-liveries-that-has-not-been-updated/refs/heads/main/livery.json").content)
+old_json = json.loads(requests.get(f"https://raw.githubusercontent.com/CCA131488/
+GeoFS-liveries-that-has-not-been-updated/{commit_id}/livery.json").content)
+keys = new_json["aircrafts"].keys()
 
-# 获取最新的 livery.json 和旧版本的 livery.json
-new_json = json.loads(requests.get(
-    "https://raw.githubusercontent.com/CCA131488/GeoFS-liveries-that-has-not-been-updated/main/livery.json"
-).content)
-
-if commit_id:
-    try:
-        old_json = json.loads(requests.get(
-            f"https://raw.githubusercontent.com/CCA131488/GeoFS-liveries-that-has-not-been-updated/{commit_id}/livery.json"
-        ).content)
-    except:
-        old_json = {"aircrafts": {}}
-else:
-    old_json = {"aircrafts": {}}
-
-# 数字表情映射
 num_map = {
     "0": ":zero:", "1": ":one:", "2": ":two:", "3": ":three:", "4": ":four:",
     "5": ":five:", "6": ":six:", "7": ":seven:", "8": ":eight:", "9": ":nine:"
@@ -38,57 +23,50 @@ num_map = {
 def emoji_number(n):
     return ''.join(num_map[d] for d in str(n))
 
-# 查找新增的涂装
 diff_data = []
+for plane in keys:
+    addition = []
+    for livery in new_json["aircrafts"][plane]["liveries"]:
+        try: 
+            if not livery in old_json["aircrafts"][plane]["liveries"]:
+                addition.append(livery)
+        except KeyError:
+            addition.append(livery)
+    try:
+        data = {"name": new_json["aircrafts"][plane]["name"], "addition": addition}
+    except KeyError:
+        data = {"name": plane, "addition": addition}
+    if addition:
+        diff_data.append(data)
+
+print(diff_data)
+
 total = 0
 
-for plane, plane_data in new_json["aircrafts"].items():
-    addition = []
-
-    for livery in plane_data["liveries"]:
-        if livery not in old_json.get("aircrafts", {}).get(plane, {}).get("liveries", []):
-            addition.append(livery)
-
-    if addition:
-        diff_data.append({
-            "name": plane_data.get("name", plane),
-            "addition": addition
-        })
-        total += len(addition)
-
-# 使用纯色 #242429
+# 关键：纯色 #242429
 color = int("242429", 16)
 
-# 如果有新增涂装，发送到 Discord
 if diff_data:
-    webhook = DiscordWebhook(url=WEBHOOK)
-    embed = DiscordEmbed(title="livery update", color=color)
+    webhook = DiscordWebhook(url=LIVERY_UPDATE_WEBHOOK)
+    embed = DiscordEmbed(title=f"livery update", color=color)
     webhook.add_embed(embed)
     webhook.execute()
 
     for plane in diff_data:
-        webhook = DiscordWebhook(url=WEBHOOK)
+        webhook = DiscordWebhook(url=LIVERY_UPDATE_WEBHOOK)
         embed = DiscordEmbed(color=color)
-
-        text = ""
-        for l in plane["addition"]:
-            text += f'{l.get("name")} *by: {l.get("credits","??")}*\n'
-
-        embed.add_embed_field(name=plane["name"], value=text.strip(), inline=False)
+        livery_list = ""
+        for livery in plane["addition"]:
+            total += 1
+            try:
+                livery_list += f'{livery["name"]} *by: {livery["credits"]}*\n'
+            except KeyError:
+                livery_list += f'{livery["name"]} *by: ??*\n'
+        embed.add_embed_field(name=plane["name"], value=livery_list.strip(), inline=False)
         webhook.add_embed(embed)
         webhook.execute()
 
-    # 添加 Total 数字的 embed
-    webhook = DiscordWebhook(url=WEBHOOK)
-    embed = DiscordEmbed(title="Total", color=color)
-    embed.add_embed_field(name="Number of new liveries", value=emoji_number(total), inline=False)
+    webhook = DiscordWebhook(url=LIVERY_UPDATE_WEBHOOK)
+    embed = DiscordEmbed(title=f"Total: {emoji_number(total)}", color=color)
     webhook.add_embed(embed)
     webhook.execute()
-
-else:
-    print("No new liveries found.")
-
-# 更新 commit.txt
-os.makedirs(".webhook", exist_ok=True)
-with open(".webhook/commit.txt", "w") as f:
-    f.write(os.environ.get("GITHUB_SHA", "main"))
